b35e413847d01daa063a8c3e6af60d6fe10fe480
vivianngan <99743770+vivianngan@users.noreply.github.com>
2024-03-15 18:26:13 -0400

    Add files via upload

---
+++ q1.sql
@@ -0,0 +1,12 @@
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    enrollment_date DATE
+);
+
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
